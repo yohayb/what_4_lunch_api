9ef80ad522b65ec67fd8e0ec903d72c2c32730ee
Yohay Berger <yohayster@gmail.com>
2019-01-15 09:27:04 -0500

    changed weather routes to return weather object

--- api/conrollers/weatherController.js
+++ api/conrollers/weatherController.js
@@ -3,13 +3,13 @@ import * as service from '../services/weather-provider-api';
 export async function getWeatherByZip(req, res) {
     const zip = req.params.zipCode;
     const weather = await service.getWeatherByZip(zip);
-    res.json({ weather: weather });
+    res.json(weather);
 }
 
 export async function getWeatherByLatLon(req, res) {
     const coord = {lat: req.params.lat, lon:req.params.lon}
 
     const weather = await service.getWeatherByZip(...coord);
-    res.json({ weather: weather });
+    res.json(weather);
 }
 
