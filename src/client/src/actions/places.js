124bdc8afa5311cfdebd6b14b6caacf669490b57
Yohay Berger <Yohay_Berger@ncci.com>
2019-02-01 12:44:03 -0500

    fixed add/delete

--- src/client/src/actions/places.js
+++ src/client/src/actions/places.js
@@ -16,11 +16,11 @@ export const getPlacesSuccess = places => ({ type: GET_PLACES_SUCCESS, places })
 export const getPlacesFailed = error => ({ type: GET_PLACES_FAILURE, error });
 
 // Add place
-export const addPlace = () => ({ type: ADD_PLACE_REQUEST });
+export const addPlace = (place) => ({ type: ADD_PLACE_REQUEST, place });
 export const addPlaceSuccess = place => ({ type: ADD_PLACE_SUCCESS, place });
 export const addPlaceFailed = error => ({ type: ADD_PLACE_FAILURE, error });
 
 // Delete place
-export const deletePlace = () => ({ type: DELETE_PLACE_REQUEST });
-export const deletePlaceSuccess = places => ({ type: DELETE_PLACE_SUCCESS, places });
+export const deletePlace = (name) => ({ type: DELETE_PLACE_REQUEST, name });
+export const deletePlaceSuccess = place => ({ type: DELETE_PLACE_SUCCESS, place });
 export const deletePlaceFailed = error => ({ type: DELETE_PLACE_FAILURE, error });
